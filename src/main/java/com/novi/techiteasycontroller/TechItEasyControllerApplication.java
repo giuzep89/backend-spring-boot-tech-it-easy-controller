6a2cb02f8be92fce9cf27010cc125dbaa6c7684f
giuzep89 <g.rinaldimusic@gmail.com>
2025-09-21 11:44:16 +0200

    Initial commit

---
+++ src/main/java/com/novi/techiteasycontroller/TechItEasyControllerApplication.java
@@ -0,0 +1,13 @@
+package com.novi.techiteasycontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechItEasyControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechItEasyControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/novi/techiteasycontroller/TechItEasyControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novi.techiteasycontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechItEasyControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
