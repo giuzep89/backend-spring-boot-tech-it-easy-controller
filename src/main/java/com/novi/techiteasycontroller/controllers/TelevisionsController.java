88f89492d6591a11c1000973f9fccd65af87a2e2
giuzep89 <g.rinaldimusic@gmail.com>
2025-09-28 13:32:01 +0200

    Database dependencies added, database configured, Repository added, Television class refactored

--- src/main/java/com/novi/techiteasycontroller/controllers/TelevisionsController.java
+++ src/main/java/com/novi/techiteasycontroller/controllers/TelevisionsController.java
@@ -5,20 +5,28 @@ import com.novi.techiteasycontroller.exceptions.RecordNotFoundException;
 import com.novi.techiteasycontroller.models.Television;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.novi.techiteasycontroller.repositories.TelevisionRepository;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/televisions")
 public class TelevisionsController {
-    Map<Integer, Television> databaseOfTVs = new HashMap<Integer,Television>();
 
-    @GetMapping("/tvs")
+    private final TelevisionRepository televisionRepository;
+
+    public TelevisionsController(TelevisionRepository televisionRepository){
+        this.televisionRepository = televisionRepository;
+    }
+
+    //TODO: Replace fake database with TelevisionRepository!!
+
+    @GetMapping
     public ResponseEntity<Map<Integer, Television>> getAllTVs(){
         return ResponseEntity.ok(this.databaseOfTVs);
     }
 
-    @GetMapping("/tvs/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Television> getOneTV(@PathVariable int id) {
         if (databaseOfTVs.containsKey(id)) {
             Television tv = databaseOfTVs.get(id);
@@ -28,7 +36,7 @@ public class TelevisionsController {
         }
     }
 
-    @PostMapping("/tvs")
+    @PostMapping
     public ResponseEntity<String> addTV(@RequestBody Television tv){
         if(tv.getBrand().length() > 20){
             throw new NameTooLongException("The brand name you provided is too long");
@@ -38,7 +46,7 @@ public class TelevisionsController {
         }
     }
 
-    @PutMapping("/tvs/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<String> updateTVPrice(@PathVariable int id, @RequestBody Television updatedTV){
         if(databaseOfTVs.containsKey(id)){
             Television foundTV = databaseOfTVs.get(id);
@@ -49,7 +57,7 @@ public class TelevisionsController {
         }
     }
 
-    @DeleteMapping("/tvs/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteTV(@PathVariable int id){
         if(!databaseOfTVs.containsKey(id)){
             throw new RecordNotFoundException("The TV with id " + id + " was not found.");
--- src/main/java/com/novi/techiteasycontroller/models/Television.java
+++ src/main/java/com/novi/techiteasycontroller/models/Television.java
@@ -1,27 +1,71 @@
 package com.novi.techiteasycontroller.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="televisions")
 public class Television {
-    private int id;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String type;
     private String brand;
-    private String model;
+    private String name;
     private double price;
+    private double availableSize;
+    private int refreshRate;
+    private String screenType;
+    private String screenQuality;
+    private boolean smartTv;
+    private boolean wifi;
+    private boolean voiceControl;
+    private boolean hdr;
+    private boolean bluetooth;
+    private boolean ambiLight;
+    private int originalStock;
+    private int sold;
 
     public Television() {
     }
 
-    public Television(int id, String brand, String model, double price) {
+    public Television(Long id, String type, String brand, String name, double price, double availableSize, int refreshRate, String screenType, String screenQuality, boolean smartTv, boolean wifi, boolean voiceControl, boolean hdr, boolean bluetooth, boolean ambiLight, int originalStock, int sold) {
         this.id = id;
+        this.type = type;
         this.brand = brand;
-        this.model = model;
+        this.name = name;
         this.price = price;
+        this.availableSize = availableSize;
+        this.refreshRate = refreshRate;
+        this.screenType = screenType;
+        this.screenQuality = screenQuality;
+        this.smartTv = smartTv;
+        this.wifi = wifi;
+        this.voiceControl = voiceControl;
+        this.hdr = hdr;
+        this.bluetooth = bluetooth;
+        this.ambiLight = ambiLight;
+        this.originalStock = originalStock;
+        this.sold = sold;
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public String getBrand() {
@@ -32,12 +76,12 @@ public class Television {
         this.brand = brand;
     }
 
-    public String getModel() {
-        return model;
+    public String getName() {
+        return name;
     }
 
-    public void setModel(String model) {
-        this.model = model;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public double getPrice() {
@@ -46,5 +90,101 @@ public class Television {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public double getAvailableSize() {
+        return availableSize;
+    }
+
+    public void setAvailableSize(double availableSize) {
+        this.availableSize = availableSize;
+    }
+
+    public int getRefreshRate() {
+        return refreshRate;
+    }
+
+    public void setRefreshRate(int refreshRate) {
+        this.refreshRate = refreshRate;
+    }
+
+    public String getScreenType() {
+        return screenType;
+    }
+
+    public void setScreenType(String screenType) {
+        this.screenType = screenType;
+    }
+
+    public String getScreenQuality() {
+        return screenQuality;
+    }
+
+    public void setScreenQuality(String screenQuality) {
+        this.screenQuality = screenQuality;
+    }
+
+    public boolean isSmartTv() {
+        return smartTv;
+    }
+
+    public void setSmartTv(boolean smartTv) {
+        this.smartTv = smartTv;
+    }
+
+    public boolean isWifi() {
+        return wifi;
+    }
+
+    public void setWifi(boolean wifi) {
+        this.wifi = wifi;
+    }
+
+    public boolean isVoiceControl() {
+        return voiceControl;
+    }
+
+    public void setVoiceControl(boolean voiceControl) {
+        this.voiceControl = voiceControl;
+    }
+
+    public boolean isHdr() {
+        return hdr;
+    }
+
+    public void setHdr(boolean hdr) {
+        this.hdr = hdr;
+    }
+
+    public boolean isBluetooth() {
+        return bluetooth;
+    }
+
+    public void setBluetooth(boolean bluetooth) {
+        this.bluetooth = bluetooth;
+    }
+
+    public boolean isAmbiLight() {
+        return ambiLight;
+    }
+
+    public void setAmbiLight(boolean ambiLight) {
+        this.ambiLight = ambiLight;
+    }
+
+    public int getOriginalStock() {
+        return originalStock;
+    }
+
+    public void setOriginalStock(int originalStock) {
+        this.originalStock = originalStock;
+    }
+
+    public int getSold() {
+        return sold;
+    }
+
+    public void setSold(int sold) {
+        this.sold = sold;
     }
 }
