2ec94820a83caa62c2e25c18cb9211596f66e43f
giuzep89 <g.rinaldimusic@gmail.com>
2025-09-29 13:03:56 +0200

    Model/Repository exercise completed (except bonus)

--- src/main/java/com/novi/techiteasycontroller/controllers/TelevisionsController.java
+++ src/main/java/com/novi/techiteasycontroller/controllers/TelevisionsController.java
@@ -7,7 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.novi.techiteasycontroller.repositories.TelevisionRepository;
 
-import java.util.Map;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/televisions")
@@ -19,51 +20,54 @@ public class TelevisionsController {
         this.televisionRepository = televisionRepository;
     }
 
-    //TODO: Replace fake database with TelevisionRepository!!
-
     @GetMapping
-    public ResponseEntity<Map<Integer, Television>> getAllTVs(){
-        return ResponseEntity.ok(this.databaseOfTVs);
+    public ResponseEntity<List<Television>> getAllTVs(){
+        return ResponseEntity.ok(televisionRepository.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Television> getOneTV(@PathVariable int id) {
-        if (databaseOfTVs.containsKey(id)) {
-            Television tv = databaseOfTVs.get(id);
-            return ResponseEntity.ok(tv);
+    public ResponseEntity<Television> getOneTV(@PathVariable Long id) {
+        Optional<Television> foundTV = televisionRepository.findById(id);
+
+        if (foundTV.isPresent()) {
+            return ResponseEntity.ok(foundTV.get());
         } else {
             throw new RecordNotFoundException("The TV with id " + id + " was not found.");
         }
     }
 
     @PostMapping
-    public ResponseEntity<String> addTV(@RequestBody Television tv){
+    public ResponseEntity<Television> addTV(@RequestBody Television tv){
         if(tv.getBrand().length() > 20){
             throw new NameTooLongException("The brand name you provided is too long");
         } else {
-            this.databaseOfTVs.put(tv.getId(), tv);
-            return ResponseEntity.created(null).body("television");
+            this.televisionRepository.save(tv);
+            return ResponseEntity.created(null).body(tv);
         }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateTVPrice(@PathVariable int id, @RequestBody Television updatedTV){
-        if(databaseOfTVs.containsKey(id)){
-            Television foundTV = databaseOfTVs.get(id);
-            foundTV.setPrice(updatedTV.getPrice());
-            return ResponseEntity.noContent().build();
+    public ResponseEntity<Television> updateTVPrice(@PathVariable Long id, @RequestBody Television tvWithNewPrice){
+        Optional<Television> possibleTV = televisionRepository.findById(id);
+
+        if(possibleTV.isPresent()){
+            Television tvToUpdate = possibleTV.get();
+            tvToUpdate.setPrice(tvWithNewPrice.getPrice());
+            televisionRepository.save(tvToUpdate);
+            return ResponseEntity.ok(tvToUpdate);
         } else {
             throw new RecordNotFoundException("The TV with id " + id + " was not found.");
         }
     }
 
+    // In this one I wanted to experiment with using the .orElseThrow(() method instead of using
+    // an if-else statement. It's much neater this way!
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteTV(@PathVariable int id){
-        if(!databaseOfTVs.containsKey(id)){
-            throw new RecordNotFoundException("The TV with id " + id + " was not found.");
-        } else {
-            databaseOfTVs.remove(id);
-            return ResponseEntity.noContent().build();
-        }
+    public ResponseEntity<Television> deleteTV(@PathVariable Long id) {
+        Television tvToDelete = televisionRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException("The TV with id " + id + " was not found."));
+        televisionRepository.delete(tvToDelete);
+        return ResponseEntity.noContent().build();
     }
+
 }
